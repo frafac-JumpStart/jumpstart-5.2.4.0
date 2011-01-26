7bf22602de241a72cda1f31d0bcf53192106308b
frafac <fra.facon@gmail.com>
2011-01-26 23:03:58 +0100

    @Import(library= instead of @IncludeJavaScriptLibrary

--- web/src/main/java/jumpstart/web/pages/examples/input/NoValidationBubbles1.java
+++ web/src/main/java/jumpstart/web/pages/examples/input/NoValidationBubbles1.java
@@ -1,11 +1,12 @@
 package jumpstart.web.pages.examples.input;
 
+import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.IncludeJavaScriptLibrary;
 import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
 
 /* This annotation tells Tapestry to declare the js file in the page so that the browser will pull it in. */
-@IncludeJavaScriptLibrary({"context:js/validators.js", "context:js/custom_error.js"})
+@Import(library={"context:js/validators.js", "context:js/custom_error.js"})
 public class NoValidationBubbles1 {
 
 	// Screen fields
