a16f6b76f8b8448d284a4ab506768116515ecc2b
frafac <fra.facon@gmail.com>
2011-03-05 23:10:50 +0100

    validation on ajaxform

--- web/src/main/java/jumpstart/web/pages/examples/javascript/AJAXForm.java
+++ web/src/main/java/jumpstart/web/pages/examples/javascript/AJAXForm.java
@@ -2,8 +2,12 @@ package jumpstart.web.pages.examples.javascript;
 
 import java.util.Date;
 
-import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.ajax.MultiZoneUpdate;
+import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.DateField;
+import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.corelib.components.TextField;
 import org.apache.tapestry5.corelib.components.Zone;
 
 public class AJAXForm {
@@ -15,11 +19,30 @@ public class AJAXForm {
 
 	@Property
 	private String _lastName;
+	
+	@Property
+	private Date _birthday;
 
 	// Generally useful bits and pieces
+	
+	@Component
+	private Form _ajaxForm;
+    
+	@Component(id = "birthday",parameters ={"validate=required"})
+	private DateField df;
+    
+	@Component(id = "firstName")
+	private TextField _firstNameField;
+    
+	@Component(id = "lastName")
+	private TextField _lastNameField;
 
-	@InjectComponent
+	@Component(id="nameZone")
 	private Zone _nameZone;
+	
+	@Component(id="formZone")
+	private Zone _formZone;
+	
 	
 	// The code
 	
@@ -29,9 +52,30 @@ public class AJAXForm {
 			_lastName = "Dumpty";
 		}
 	}
+	
+	void onValidateForm() {
+		
+		if (_firstName == null || _firstName.trim().equals("")) {
+			_ajaxForm.recordError(_firstNameField, "First Name is required.");
+		}
+		if (_lastName == null || _lastName.trim().equals("")) {
+			_ajaxForm.recordError(_lastNameField, "Last Name is required.");
+		}
+		Date now = new Date(); 
+		if(_birthday.after(now))
+		{
+			_ajaxForm.recordError(df, "invalid birthday");
+		}
+	}
+
 
 	Object onSuccess() {
-		return _nameZone.getBody();
+		return  new MultiZoneUpdate(_nameZone).add(_formZone);
+	}
+	
+	Object onFailure() {
+		return  new MultiZoneUpdate(_nameZone).add(_formZone);
+		
 	}
 
 	public String getName() {
